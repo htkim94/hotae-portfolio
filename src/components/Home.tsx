44168c5cc9c81537b5d5eeb5dcc1813ce6f383a4
Hotae Kim <hotae.kim.94@gmail.com>
2023-06-10 03:01:21 -0400

    add bottom down arrow with animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,16 +4,22 @@ function Home(
   return (
     <section
       id="home"
-      className="h-screen max-h-home-hero w-full bg-slate-500 flex flex-col justify-center items-center"
+      className="relative h-screen max-h-[70rem] w-full bg-slate-500 flex flex-col justify-center items-center"
       // ref={el => appSections[0] = el}
     >
       <div className="flex flex-col justify-between items-center gap-10">
-        <h1 className="relative w-max text-7xl font-ubuntu-mono font-bold uppercase">
+        <h1 className="relative w-max md:text-5xl lg:text-6xl xl:text-7xl font-ubuntu-mono font-bold uppercase">
           HELLO, MY NAME IS HOTAE KIM.
         </h1>
-        <h2 className="text-3xl tracking-wider">
+        <h2 className="lg:text-2xl xl:text-3xl tracking-wider">
           SOFTWARE ENGINEER, FRONT END WEB DEVELOPER
         </h2>
+      </div>
+      <div
+        id="home-down-arrow"
+        className="text-4xl rotate-90 absolute"
+      >
+        &gt;
       </div>
     </section>
   );
