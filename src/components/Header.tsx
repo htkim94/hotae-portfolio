eacbc7e8ee5bc7a357e7359c70a37760334d0c02
Hotae Kim <hotae.kim.94@gmail.com>
2023-06-17 18:22:43 -0400

    add hamburger menu icon for mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo.tsx";
 import NavItem from "./NavItem.tsx";
 import { PageSections } from '../interfaces/PageSections.interface.ts';
+import { ReactComponent as BurgerMenu } from '../assets/icons/burger-menu-svgrepo-com.svg';
 
 interface HeaderProps {
   sections: PageSections["list"];
@@ -10,10 +11,13 @@ function Header({ sections }: HeaderProps) {
   return (
     <header
       id="header"
-      className="fixed z-50 w-full flex justify-between items-center px-10 py-5 bg-discord-900 text-white"
+      className="fixed z-50 w-full flex justify-between items-center px-6 md:px-10 py-5 bg-discord-900 text-white"
     >
       <Logo section={sections[0]} />
-      <div className="flex gap-8 text-xl tracking-widest">
+      <div className="flex justify-between items-center lg:hidden w-9 h-9 text-cyan-400">
+        <BurgerMenu />
+      </div>
+      <div className="hidden lg:flex gap-8 text-xl tracking-widest">
         <NavItem type="home" section={sections[0]} />
         <NavItem type="about" section={sections[1]} />
         <NavItem type="projects" section={sections[2]} />
