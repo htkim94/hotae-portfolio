91e528ad480771a68661bc80723fd90674bfdfe4
Hotae Kim <hotae.kim.94@gmail.com>
2023-06-19 15:44:57 -0400

    adjust home font size for mobile width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,26 +9,31 @@ function Home({ section }: HomeProps) {
   return (
     <section
       id="home"
-      className="relative h-screen max-h-[70rem] w-full bg-discord-800 flex flex-col justify-center items-center"
+      className="relative h-screen max-h-[45rem] md:max-h-[70rem] w-full bg-discord-800 flex flex-col justify-center items-center"
       // ref={el => appSections[0] = el}
     >
-      <div className="flex flex-col justify-between items-center gap-20">
+      <div className="flex flex-col justify-between items-center gap-12 md:gap-20">
         <div className="flex flex-col justify-between items-center gap-10 text-white">
-          <h1 className="relative w-max md:text-5xl lg:text-6xl xl:text-7xl font-ubuntu-mono font-bold uppercase">
+          <h1 className="relative w-max text-2xl md:text-5xl lg:text-6xl xl:text-7xl font-ubuntu-mono font-bold uppercase">
             HELLO, MY NAME IS <span className="text-cyan-400">HOTAE KIM</span>.
           </h1>
-          <h2 className="md:text-2xl xl:text-3xl tracking-wider">
-            SOFTWARE ENGINEER, FRONT END WEB DEVELOPER
-          </h2>
+          <div className="flex flex-col md:flex-row gap-3 text-lg md:text-2xl xl:text-3xl tracking-wider text-center">
+            <h2>
+              SOFTWARE ENGINEER<span className="hidden md:inline">,</span>
+            </h2>
+            <h2>
+              FRONT END WEB DEVELOPER
+            </h2>
+          </div>
         </div>
-        <div className="flex justify-between gap-12">
+        <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-12">
           <Button section={section}>PROJECTS</Button>
           <Button isResume={true}>RESUME</Button>
         </div>
       </div>
       <div
         id="home-down-arrow"
-        className="text-3xl text-white rotate-90 absolute"
+        className="text-2xl md:text-3xl text-white rotate-90 absolute"
       >
         &gt;
       </div>
